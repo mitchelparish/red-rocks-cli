5d47ac748b85d7205b15361cf11aac26e3e799da
Mitchel Parish <mitchelparish@gmail.com>
2019-02-05 21:33:54 +0000

    cleaned up setup files

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@ require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
+
 require_relative '../lib/red_rocks/scraper'
 require_relative '../lib/red_rocks/concert'
 require_relative '../lib/red_rocks/cli'
