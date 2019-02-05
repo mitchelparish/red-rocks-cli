d890064a85fb683fcaabd9a1d210b494eb2605d6
Mitchel Parish <mitchelparish@gmail.com>
2019-02-05 23:32:48 +0000

    built concert class

--- lib/red_rocks/concert.rb
+++ lib/red_rocks/concert.rb
@@ -1,8 +1,26 @@
 require 'pry'
 
 class RedRocks::Concert
+  attr_accessor :name, :opener, :date, :time, :doors, :tickets_URL
 
+  @@all = []
 
+  def initialize(name, opener, date, time, doors, tickets_URL)
+    @name = name
+    @opener = opener
+    @date = date
+    @time = time
+    @doors = doors
+    @tickets_URL = tickets_URL
+    @@all << self
+    #separate save method?
+  end
 
+  def self.all
+    @@all
+  end
 
+  def self.find_by_index(index)
+    @@all[index]
+  end 
 end
