f4d14f5ec618882bf5105e3060bdaf311b818ac2
Mitchel Parish <mitchelparish@gmail.com>
2019-02-05 22:52:44 +0000

    built out cli methods

--- lib/red_rocks/cli.rb
+++ lib/red_rocks/cli.rb
@@ -3,24 +3,28 @@ require 'colorize'
 class RedRocks::CLI
 
   def call
+    #RedRocks::Scraper.scrape_concerts
     list_concerts
     menu
+    goodbye
+
   end
 
   def list_concerts
     puts ""
-    puts "Welcome to Red Rocks Amphitheatre!!".red
+    puts "// Welcome to Red Rocks Park and Amphitheatre!! //".red
     puts ""
-    puts "Please select a concert below:"
-    puts ""
-    puts "1. Concert_name1"
-    puts "2. Concert_name2"
-    puts "3. Concert_name3"
-    puts ""
+    #list all instances of the Concert class with corresponding number
   end
 
   def menu
-    puts "Please enter the concert number you wish to see:"
+    input = nil
+
+    while input != exit
+      puts "Which concert would you like to go to? If you're lame, type exit."
+      input = gets.strip
   end
 
+  def goodbye
+  end
 end
