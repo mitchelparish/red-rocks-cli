b6f4d24a758104104b3459590760bfdc7a54ed2c
Mitchel Parish <mitchelparish@gmail.com>
2019-02-05 23:08:37 +0000

    built cli methods

--- lib/red_rocks/cli.rb
+++ lib/red_rocks/cli.rb
@@ -21,10 +21,24 @@ class RedRocks::CLI
     input = nil
 
     while input != exit
-      puts "Which concert would you like to go to? If you're lame, type exit."
+      puts "Please select the concert number you wish to see. To bounce, type exit."
       input = gets.strip
+
+      if input.to_i > 0
+        concert_choice = RedRocks::Concert.find_by_index(input.to_i - 1)
+        if concert_choice = nil
+          puts "Be groovy or type exit to leave, man."
+          puts "Please select the concert number you wish to see."
+        else
+          puts "Here's some more information about the selected show..."
+        end
+      else
+        puts "Please try again."
+      end
+    end
   end
 
   def goodbye
+    puts "Rock on!"
   end
-end
+end 
