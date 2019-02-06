fcf4c1e1cbb32ce152f9188ff242de5a989ff3c0
Mitchel Parish <mitchelparish@gmail.com>
2019-02-06 21:03:49 +0000

    added comments

--- lib/red_rocks/scraper.rb
+++ lib/red_rocks/scraper.rb
@@ -1,14 +1,12 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
+require 'pry' #remove before submission.
+require 'nokogiri' #remove before submission, already required in environment.
+require 'open-uri' #remove before submission, already required in environment.
 
-# class RedRocks::Scraper
+# class RedRocks::Scraper - revert back before submission.
 class Scraper
 
   def self.scrape
     doc = Nokogiri::HTML(open("https://www.redrocksonline.com/events/category/Concerts"))
-
-    binding.pry
 
     doc.css("div.entry.event-icons.clearfix").each do |concert|
       name = concert.css("h3").text
@@ -17,8 +15,9 @@ class Scraper
       time = concert.css("span.m-doors").text.gsub("Doors","").strip
       tickets_URL = concert.css("div.buttons a").attr("href").text
       # RedRocks::Concert.new(name, opener, date, time, tickets_URL)
+      binding.pry #remove before submission. 
     end
   end
 end
 
-Scraper.scrape
+Scraper.scrape #remove before submission.
