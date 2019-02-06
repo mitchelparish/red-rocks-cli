276ca39d67c5e1b172f82cb8ff06b1789aaae2e6
Mitchel Parish <mitchelparish@gmail.com>
2019-02-06 19:34:25 +0000

    scraper binding

--- lib/red_rocks/scraper.rb
+++ lib/red_rocks/scraper.rb
@@ -1,19 +1,20 @@
-# require 'pry'
+require 'pry'
 # require 'nokogiri'
 # require 'open-uri'
 
-class RedRocks::Scraper
+# class RedRocks::Scraper
+class Scraper
 
   def self.scrape_concerts
     doc = Nokogiri::HTML(open("https://www.redrocksonline.com/events/category/Concerts"))
+
+    binding.pry
 
     doc.css("div.entry.event-icons.clearfix").each do |concert|
       name = concert.css("h3").text
       opener = concert.css("h4").text
 
-      binding.pry
-
-      #
+      
       # RedRocks::Concert.new(name, opener, date, time, doors, tickets_URL)
     end
   end
