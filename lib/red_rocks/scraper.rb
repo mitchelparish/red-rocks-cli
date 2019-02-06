2008898775ac7c0a7e6a60f83701812d7a461927
Mitchel Parish <mitchelparish@gmail.com>
2019-02-06 16:41:58 +0000

    targeting scraper

--- lib/red_rocks/scraper.rb
+++ lib/red_rocks/scraper.rb
@@ -5,7 +5,14 @@ require 'open-uri'
 class RedRocks::Scraper
 
   def self.scrape_concerts
-    page = Nokogiri::HTML(open("https://www.redrocksonline.com/events/category/Concerts"))
+    doc = Nokogiri::HTML(open("https://www.redrocksonline.com/events/category/Concerts"))
+
+    doc.css("div.entry.event-icons.clearfix").each do |concert|
+      name = concert.css("h3").text
 
 
-end
+
+      RedRocks::Concert.new(name, opener, date, time, doors, tickets_URL)
+    end
+  end
+end 
