5153f22a2fcda11d70fffb2a64831e6d68d4db7d
Mitchel Parish <mitchelparish@gmail.com>
2019-02-06 20:53:09 +0000

    added tickets_URL

--- lib/red_rocks/scraper.rb
+++ lib/red_rocks/scraper.rb
@@ -15,7 +15,7 @@ class Scraper
       opener = concert.css("h4").text
       date = concert.css("span.m-date-value.m-value").text
       time = concert.css("span.m-doors").text.gsub("Doors","").strip
-      tickets_URL = concert.css("div.buttons a").map { |link| link['href'] }
+      tickets_URL = concert.css("div.buttons a").attr("href").text
 
 
       # RedRocks::Concert.new(name, opener, date, time, tickets_URL)
